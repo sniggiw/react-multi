e85dff623836e881e17972df454d6bff62649a76
wengweijian <sniggiw@qq.com>
2024-08-26 17:41:43 +0800

    feat:arrange code

--- src/modules/fiveFortune/views/Index/index.jsx
+++ src/modules/fiveFortune/views/Index/index.jsx
@@ -4,12 +4,23 @@ import { drawLine } from "@utils";
 import Marquee from "@components/Marquee";
 import styles from "./styles.module.less";
 
+const STAR_OBJ = {
+    pojun: "破军",
+    wuqu: "武曲",
+    lianzhen: "廉贞",
+    wenqu: "文曲",
+    lucun: "禄存",
+    jumen: "巨门",
+    tanlang: "贪狼",
+    dixing: "帝星",
+};
+
 export default function Index() {
     const [lineState, setLineState] = useState([]);
 
     const getTheLineState = () => {
         for (let i = 0; i < 6; i++) {
-            const res = drawLine(`star${i}`, `star${i + 1}`);
+            const res = drawLine(`star${i + 1}`, `star${i + 2}`);
             setLineState((prev) => [...prev, res]);
         }
     };
@@ -37,59 +48,49 @@ export default function Index() {
 
                     <div className={styles["anime-box"]}>
                         <div className={styles["star-box"]}>
-                            <img id="star0" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star1" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star2" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star3" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star4" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star5" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star6" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
-                            <img id="star7" className={styles["star"]} src="../../assets/imgs/index/star.png" alt="star" />
+                            {Array.from({ length: 8 }).map((_, index) => {
+                                return (
+                                    <img
+                                        id={`star${index + 1}`}
+                                        key={`star${index + 1}`}
+                                        className={styles["star"]}
+                                        src="../../assets/imgs/index/star.png"
+                                        alt="star"
+                                    />
+                                );
+                            })}
                         </div>
-                        id=""
                         <div className={styles["star-text"]}>
-                            <img id="text0" className={styles["text"]} src="../../assets/imgs/index/star-text_pojun.png" alt="破军" />
-                            <img id="text1" className={styles["text"]} src="../../assets/imgs/index/star-text_wuqu.png" alt="武曲" />
-                            <img id="text2" className={styles["text"]} src="../../assets/imgs/index/star-text_lianzhen.png" alt="廉贞" />
-                            <img id="text3" className={styles["text"]} src="../../assets/imgs/index/star-text_wenqu.png" alt="文曲" />
-                            <img id="text4" className={styles["text"]} src="../../assets/imgs/index/star-text_lucun.png" alt="禄存" />
-                            <img id="text5" className={styles["text"]} src="../../assets/imgs/index/star-text_jumen.png" alt="巨门" />
-                            <img id="text6" className={styles["text"]} src="../../assets/imgs/index/star-text_tanlang.png" alt="贪狼" />
-                            <img id="text7" className={styles["text"]} src="../../assets/imgs/index/star-text_dixing.png" alt="帝星" />
+                            {Object.entries(STAR_OBJ).map((item, index) => {
+                                return (
+                                    <img
+                                        id={`text${index + 1}`}
+                                        key={`text${index + 1}`}
+                                        className={styles["text"]}
+                                        src={`../../assets/imgs/index/star-text_${item[0]}.png`}
+                                        alt={item[1]}
+                                    />
+                                );
+                            })}
                         </div>
                         <div className={styles["star-line"]}>
                             {lineState.map((item, index) => {
-                                return <div id={`line${index}`} key={`line${index}`} style={{ ...item }}></div>;
+                                return <div id={`line${index + 1}`} key={`line${index + 1}`} style={{ ...item }}></div>;
                             })}
                         </div>
                     </div>
                 </div>
                 <div className={styles["introduce-box"]}>
-                    <img
-                        className={`${styles["introduce_img"]} ${styles["introduce_img1"]}`}
-                        src="../../assets/imgs/index/introduce_1.png"
-                        alt="图文介绍 1"
-                    />
-                    <img
-                        className={`${styles["introduce_img"]} ${styles["introduce_img2"]}`}
-                        src="../../assets/imgs/index/introduce_2.png"
-                        alt="图文介绍 2"
-                    />
-                    <img
-                        className={`${styles["introduce_img"]} ${styles["introduce_img3"]}`}
-                        src="../../assets/imgs/index/introduce_3.png"
-                        alt="图文介绍 3"
-                    />
-                    <img
-                        className={`${styles["introduce_img"]} ${styles["introduce_img4"]}`}
-                        src="../../assets/imgs/index/introduce_4.png"
-                        alt="图文介绍 4"
-                    />
-                    <img
-                        className={`${styles["introduce_img"]} ${styles["introduce_img5"]}`}
-                        src="../../assets/imgs/index/introduce_5.png"
-                        alt="图文介绍 5"
-                    />
+                    {Array.from({ length: 5 }).map((_, index) => {
+                        return (
+                            <img
+                                key={`introduce_img${index + 1}`}
+                                className={`${styles["introduce_img"]} ${styles[`introduce_img${index + 1}`]}`}
+                                src={`../../assets/imgs/index/introduce_${index + 1}.png`}
+                                alt={`图文介绍 ${index + 1}`}
+                            />
+                        );
+                    })}
                 </div>
             </main>
         </>
